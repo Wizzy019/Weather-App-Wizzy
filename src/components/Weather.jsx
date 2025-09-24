b931cbc00109d519f67b6115cec701c93f8a8539
Wizzy019 <kingwizzyndukwu@gmail.com>
2025-09-24 18:41:22 +0100

    Added a input function to make the imput field and the search icon work

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react'
+import React, {useEffect, useRef, useState} from 'react'
 import './Weather.css'
 import search_icon from '../assets/search.png'
 import clear_icon from '../assets/clear.png'
@@ -11,7 +11,7 @@ import humidity_icon from '../assets/humidity.png'
 
 const Weather = () => {
 
-
+    const inpurRef =useRef();
     const [weatherData, setWeatherData] = useState(false);
 
     const allIcons = {
@@ -32,12 +32,17 @@ const Weather = () => {
     }
 
     const search = async (city) => {
-
-        const url =`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${import.meta.env.VITE_APP_ID}`;
+            
+        if(city === ""){
+          alert("Enter city Name");
+          return;
+        }
+         
+        try{
+           const url =`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${import.meta.env.VITE_APP_ID}`;
 
           const response = await fetch(url);
           const data = await response.json();
-          console.log(data);
           const icon = allIcons[data.weather[0].icon] || clear_icon
 
           setWeatherData({
@@ -47,19 +52,26 @@ const Weather = () => {
             location: data.name,
             icon: icon
           })
+        } catch (error){
+         setWeatherData(false);
+        console.error(error);
+        }
+       
     }
 
     useEffect(() =>{
-      search("Awka")
+      search()
     },[])
 
 return (
     <div className='weather'>
         <div className="search-bar">
-            <input type="text" placeholder='search' />
-          <img src={search_icon} alt="" />
+            <input ref={inpurRef} type="text" placeholder='search' />
+          <img src={search_icon} alt="" onClick={() =>search(inpurRef.current.value)}/>
         </div>
-         <img src={weatherData.icon} alt="" className='weather-icon'/>
+        {weatherData?
+        <>
+           <img src={weatherData.icon} alt="" className='weather-icon'/>
         <p className='temperature'>{weatherData.temperature}&deg;C</p>
         <p className='location'>{weatherData.location}</p>
         <div className="weather-data">
@@ -77,8 +89,10 @@ return (
               <span>Wind Speed</span>
             </div>
           </div>
-
         </div>
+        </>:
+        <></>}
+        
     </div>
   )
 }
